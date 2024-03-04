7a26af1e025806e68c49f7846051ad54d5d8b3be
Timo Kössler <info@timokoessler.de>
2024-03-04 14:27:26 +0100

    Add license modal

--- Guard/Views/Pages/Settings.xaml.cs
+++ Guard/Views/Pages/Settings.xaml.cs
@@ -228,5 +228,35 @@ namespace Guard.Views.Pages
         {
             mainWindow.Navigate(typeof(ChangePasswordPage));
         }
+
+        private void License_Button_Click(object sender, RoutedEventArgs e)
+        {
+            _ = new Wpf.Ui.Controls.MessageBox
+            {
+                Title = "MIT License",
+                Content =
+                    @"Copyright (c) 2024 Timo Kössler and Open Source Contributors - https://github.com/timokoessler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.",
+                CloseButtonText = I18n.GetString("dialog.close"),
+                MaxWidth = 700
+            }.ShowDialogAsync();
+        }
     }
 }
