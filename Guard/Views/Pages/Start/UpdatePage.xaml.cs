c902fb452e61bf4c45739285c0ebe067c7765101
Timo Kössler <info@timokoessler.de>
2024-03-29 14:10:46 +0100

    Improve update security by validating signature

--- Guard/Views/Pages/Start/UpdatePage.xaml.cs
+++ Guard/Views/Pages/Start/UpdatePage.xaml.cs
@@ -1,9 +1,9 @@
-﻿using Guard.Core;
-using Guard.Core.Installation;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using Guard.Core;
+using Guard.Core.Installation;
 
 namespace Guard.Views.Pages.Start
 {
@@ -41,106 +41,10 @@ namespace Guard.Views.Pages.Start
             ProgressPanel.Visibility = Visibility.Visible;
 
             mainWindow.GetStatsClient()?.TrackEvent("UpdateStarted");
-            bool isPortable = InstallationInfo.IsPortable();
 
             try
             {
-                string downloadUrl = isPortable
-                    ? updateInfo.Urls.Portable
-                    : updateInfo.Urls.Installer;
-
-                string downloadFileName = Path.GetFullPath(
-                    isPortable
-                        ? Path.Combine(
-                            AppContext.BaseDirectory,
-                            $"2FAGuard-Portable-{updateInfo.Version}.zip"
-                        )
-                        : Path.Combine(
-                            Path.GetTempPath(),
-                            $"2FAGuard-Updater-{updateInfo.Version}.exe"
-                        )
-                );
-
-                Log.Logger.Information(
-                    "Downloading update from {0} to {1}",
-                    downloadUrl,
-                    downloadFileName
-                );
-                if (File.Exists(downloadFileName))
-                {
-                    File.Delete(downloadFileName);
-                }
-
-                string portableExePath = Path.Combine(
-                    AppContext.BaseDirectory,
-                    $"2FAGuard-Portable-{updateInfo.Version}.exe"
-                );
-                if (isPortable && File.Exists(portableExePath))
-                {
-                    throw new Exception(
-                        "You have already downloaded the newest portable version. Please start the new version instead of the old one."
-                    );
-                }
-
-                using var stream = await Updater.httpClient.GetStreamAsync(downloadUrl);
-                using FileStream fileStream =
-                    new(downloadFileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
-                await stream.CopyToAsync(fileStream);
-                fileStream.Close();
-                stream.Close();
-
-                string startFilePath = downloadFileName;
-                string arguments = "/SILENT";
-                if (isPortable)
-                {
-                    string extractDir = Path.Combine(
-                        AppContext.BaseDirectory,
-                        $"2FAGuard-Portable-Update-{updateInfo.Version}-Temp"
-                    );
-
-                    await Task.Run(() =>
-                    {
-                        if (Directory.Exists(extractDir))
-                        {
-                            Directory.Delete(extractDir, true);
-                        }
-                        Directory.CreateDirectory(extractDir);
-                        System.IO.Compression.ZipFile.ExtractToDirectory(
-                            downloadFileName,
-                            extractDir
-                        );
-                        string[] fileNames = Directory.GetFiles(
-                            extractDir,
-                            "*.exe",
-                            SearchOption.AllDirectories
-                        );
-                        if (fileNames.Length == 0)
-                        {
-                            throw new Exception("Did not find any executable in the zip file");
-                        }
-                        if (fileNames.Length > 1)
-                        {
-                            throw new Exception("Found more than one executable in the zip file");
-                        }
-                        File.Move(fileNames[0], portableExePath);
-                        File.Delete(downloadFileName);
-                        Directory.Delete(extractDir, true);
-                    });
-                    startFilePath = portableExePath;
-                    arguments = $"--updated-from {InstallationInfo.GetVersionString()} --portable";
-                }
-
-                using var process = new Process
-                {
-                    StartInfo = new ProcessStartInfo(startFilePath)
-                    {
-                        UseShellExecute = true,
-                        WindowStyle = ProcessWindowStyle.Normal,
-                        Arguments = arguments
-                    },
-                };
-                process.Start();
-                Application.Current.Shutdown();
+                await Updater.Update(updateInfo);
             }
             catch (Exception ex)
             {
--- Guard/publish.ps1
+++ Guard/publish.ps1
@@ -6,5 +6,5 @@ Move-Item bin\portable\2FAGuard.exe bin\portable\2FAGuard-Portable.exe -Force
 signtool.exe sign /n "Open Source Developer, Timo Kössler" /t "http://time.certum.pl/" /fd sha256 /d "2FAGuard" /du "https://2faguard.app" .\bin\publish\2FAGuard.exe .\bin\portable\2FAGuard-Portable.exe
 # Create installer
 Set-Location Installer
-#Start-Process -NoNewWindow -FilePath "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" -ArgumentList "./installer.iss"
+Start-Process -NoNewWindow -FilePath "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" -ArgumentList "./installer.iss"
 Set-Location ..
