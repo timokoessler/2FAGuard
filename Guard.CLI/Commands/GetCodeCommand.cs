6985f56be66eff6081f8156226305bdb3a819a19
Timo Kössler <info@timokoessler.de>
2024-06-09 17:15:21 +0200

    Add CLI project

---
+++ Guard.CLI/Commands/GetCodeCommand.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Guard.CLI.Commands
+{
+    internal sealed class GetCodeCommand : Command
+    {
+        public override int Execute(CommandContext context)
+        {
+            AnsiConsole.WriteLine("Hello from GetCodeCommand!");
+            return 0;
+        }
+    }
+}
---
+++ Guard.CLI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Guard.CLI.Commands;
+using Guard.Core;
+using Spectre.Console.Cli;
+using System.Globalization;
+
+namespace Guard.CLI
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            // Todo: Detect InstallationType and isPortable and set it here
+            InstallationContext.Init(InstallationType.CLASSIC_PORTABLE, false, System.Reflection.Assembly.GetExecutingAssembly().GetName().Version ?? new Version(0, 0));
+
+            var app = new CommandApp();
+            app.Configure(config =>
+            {
+                config.SetApplicationName("2fa");
+                config.SetApplicationVersion(InstallationContext.GetVersionString());
+                config.SetApplicationCulture(CultureInfo.GetCultureInfo("en"));
+                config.AddCommand<GetCodeCommand>("get")
+                    .WithDescription("Get a two-factor authentication code.");
+            });
+            
+            return app.Run(args);
+        }
+    }
+}
